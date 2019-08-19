7ad09afe8785f86197503f5feeca407390e075a8
pringgojs <odyinggo@gmail.com>
2019-08-19 09:32:59 +0700

    update skip csrf token

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,8 +14,13 @@ class VerifyCsrfToken extends BaseVerifier
     protected $except = [
         'filter/lecturer/get-modul',
         'filter/lecturer/get-matakuliah',
+        'lecturer/materi/filter',
+        'lecturer/schedule/filter',
+        'lecturer/nilai-permodul/filter',
+        'lecturer/nilai-permodul/filter',
+
+        'student/e-tugas/filter',
         'filter/student/get-modul',
         'filter/student/get-matakuliah/*',
-        'login',
     ];
 }
